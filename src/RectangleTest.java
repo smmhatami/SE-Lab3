67ab74a43c97cfd2183321c760e84b6d06e7c7d5
Seyed Mohammad Mahdi Hatami <hatamik7@gmail.com>
2023-11-10 21:13:43 +0330

    develop test for setter functions

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -49,4 +49,44 @@ public class RectangleTest {
         assertEquals(rectangle.getHeight(), 9);
     }
 
+    @Test
+    void testSetWidth(){
+        Rectangle rectangle;
+        rectangle = new Rectangle(2, 3);
+        rectanlge.setWidth(20);
+        assertEquals(rectangle.getWidth(), 20);
+        rectangle = new Rectangle(1, 1);
+        rectanlge.setWidth(3);
+        assertEquals(rectangle.getWidth(), 3);
+        rectangle = new Rectangle(150, 200);
+        rectanlge.setWidth(1);
+        assertEquals(rectangle.getWidth(), 1);
+        rectangle = new Rectangle(1, 10000);
+        rectanlge.setWidth(200);
+        assertEquals(rectangle.getWidth(), 200);
+        rectangle = new Rectangle(7, 9);
+        rectanlge.setWidth(10000);
+        assertEquals(rectangle.getWidth(), 10000);
+    }
+
+    @Test
+    void testSetHeight(){
+        Rectangle rectangle;
+        rectangle = new Rectangle(2, 3);
+        rectanlge.setHeight(20);
+        assertEquals(rectangle.getHeight(), 20);
+        rectangle = new Rectangle(1, 1);
+        rectanlge.setHeight(3);
+        assertEquals(rectangle.getHeight(), 3);
+        rectangle = new Rectangle(150, 200);
+        rectanlge.setHeight(1);
+        assertEquals(rectangle.getHeight(), 1);
+        rectangle = new Rectangle(1, 10000);
+        rectanlge.setHeight(200);
+        assertEquals(rectangle.getHeight(), 200);
+        rectangle = new Rectangle(7, 9);
+        rectanlge.setHeight(10000);
+        assertEquals(rectangle.getHeight(), 10000);
+    }
+
 }
