07f027d927fce00e66854a9879bc3e07c9e2c33d
Seyed Mohammad Mahdi Hatami <hatamik7@gmail.com>
2023-11-10 21:25:16 +0330

    develop test for side getter and setter for square

--- src/SquareTest.java
+++ src/SquareTest.java
@@ -18,4 +18,41 @@ public class SquareTest {
         square = new Square(7);
         assertEquals(square.computeArea(), 49);
     }
+
+
+    @Test
+    void testGetSide(){
+        Square square;
+        square = new Square(2);
+        assertEquals(square.getSide(), 2);
+        square = new Square(1);
+        assertEquals(square.getSide(), 1);
+        square = new Square(150);
+        assertEquals(square.getSide(), 150);
+        square = new Square(1);
+        assertEquals(square.getSide(), 1);
+        square = new Square(7);
+        assertEquals(square.getSide(), 7);
+    }
+
+    @Test
+    void testSetSide(){
+        Square square;
+        square = new Square(2);
+        rectanlge.setSide(20);
+        assertEquals(square.getSide(), 20);
+        square = new Square(1);
+        rectanlge.setSide(3);
+        assertEquals(square.getSide(), 3);
+        square = new Square(150);
+        rectanlge.setSide(1);
+        assertEquals(square.getSide(), 1);
+        square = new Square(1);
+        rectanlge.setSide(200);
+        assertEquals(square.getSide(), 200);
+        square = new Square(7);
+        rectanlge.setSide(10000);
+        assertEquals(square.getSide(), 10000);
+    }
+
 }
