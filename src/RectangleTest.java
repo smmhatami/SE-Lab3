23eb11a823d1a8d0b9d6d598814c3e418a5f382c
Seyed Mohammad Mahdi Hatami <hatamik7@gmail.com>
2023-11-10 21:09:32 +0330

    develop test for getter functions

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -18,4 +18,35 @@ public class RectangleTest {
         rectangle = new Rectangle(7, 9);
         assertEquals(rectangle.computeArea(), 63);
     }
+
+    @Test
+    void testGetWidth(){
+        Rectangle rectangle;
+        rectangle = new Rectangle(2, 3);
+        assertEquals(rectangle.getWidth(), 2);
+        rectangle = new Rectangle(1, 1);
+        assertEquals(rectangle.getWidth(), 1);
+        rectangle = new Rectangle(150, 200);
+        assertEquals(rectangle.getWidth(), 150);
+        rectangle = new Rectangle(1, 10000);
+        assertEquals(rectangle.getWidth(), 1);
+        rectangle = new Rectangle(7, 9);
+        assertEquals(rectangle.getWidth(), 7);
+    }
+
+    @Test
+    void testGetHeight(){
+        Rectangle rectangle;
+        rectangle = new Rectangle(2, 3);
+        assertEquals(rectangle.getHeight(), 3);
+        rectangle = new Rectangle(1, 1);
+        assertEquals(rectangle.getHeight(), 1);
+        rectangle = new Rectangle(150, 200);
+        assertEquals(rectangle.getHeight(), 200);
+        rectangle = new Rectangle(1, 10000);
+        assertEquals(rectangle.getHeight(), 10000);
+        rectangle = new Rectangle(7, 9);
+        assertEquals(rectangle.getHeight(), 9);
+    }
+
 }
